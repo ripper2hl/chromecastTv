e15effd008f23883db11659339f13e554ef557a7
Jesús Perales <jesus.peralesmr@uanl.edu.mx>
2015-10-30 01:26:54 -0600

    Habilitando compresion de express

--- src/server/express.js
+++ src/server/express.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
+var compression = require('compression');
 
 module.exports = function(){
 
+  app.use(compression());
   app.use(serveStatic('/home/perales/test/'));
 
   var server = app.listen(3000, function () {
